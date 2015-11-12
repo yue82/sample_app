a343de115727fb6ce4302815f127acd2b998cf5a
tktr <no.82.vuf.lab@gmail.com>
2015-11-12 15:47:34 +0900

    commit 1112

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # return full title fit page
+  def full_title(page_title)
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
 end
