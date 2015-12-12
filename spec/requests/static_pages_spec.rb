959c22d1111e7ce7f1291f44ec06516794721e16
tktr <no.82.vuf.lab@gmail.com>
2015-12-12 17:38:14 +0900

    fin 4

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -16,6 +16,21 @@ describe 'Static pages' do
 
     it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
+
+    it "should habe the right links on the layout" do
+      click_link "About"
+      expect(page).have_title(full_title('About Us'))
+      click_link "Help"
+      expect(page).to have_title(full_title('Help'))
+      click_link "Contact"
+      expect(page).to have_title(full_title('Contact'))
+      click_link "Home"
+      click_link "sample app"
+      expect(page).to have_title(full_title(''))
+      click_link "Sign up now!"
+      expect(page).to have_title(full_title('Sign up'))
+    end
+
   end
 
 
