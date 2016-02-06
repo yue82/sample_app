f8f51b999d596763e9d53e33dd83f7b8857a6dd5
tktr <no.82.vuf.lab@gmail.com>
2016-02-07 02:18:08 +0900

    done section 6 exercise

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe User do
 
   before { @user = User.new(name: "Example User", email: "user@example.com",
-                            password: "foobar", password_confimation: "foobar") }
+                            password: "foobar", password_confirmation: "foobar") }
 
   subject { @user }
 
@@ -60,20 +60,27 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
   describe "when password is not present" do
     before do
-      @user = User,new(name: "Example User", email: "user@example.com",
-                       password: " ", password_confimation: " ")
+      @user = User.new(name: "Example User", email: "user@example.com",
+                       password: " ", password_confirmation: " ")
     end
     it { should_not be_valid }
   end
 
   describe "when password doesn't match confirmation" do
-    before { @user.password_confimation = "mismatch" }
+    before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
-
-
 
   describe "return value of authenticate method" do
     before { @user.save }
@@ -92,8 +99,8 @@ describe User do
   end
 
   describe "with a password that's too short" do
-    before { @user.password = @user.password_confimation = "a" * 5 }
-    it { should be_valid }
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should_not be_valid }
   end
 
 end
