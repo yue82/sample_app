96c575a13ccbe0c32750a6d43685c09efdd65f9b
tktr <no.82.vuf.lab@gmail.com>
2016-02-12 16:08:01 +0900

    add 7 section exercise

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -34,7 +34,38 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+
     end
+  end
+
+  describe "signup" do
+    before { visit signup_path }
+    it { should have_title('Sign up') }
+    it { should_not have_content('error') }
+
+    describe "with invalid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foob"
+        fill_in "Confirmation", with: "foob"
+      end
+
+      describe "after submission" do
+        before { click_button "Create my account" }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
+    end
+
   end
 
 end
